0cd6ae570a0acf6c8cf91963d8a7ff87bef71755
Pashkevich Tania <pashkevichtania2004@gmail.com>
2022-09-22 17:30:35 +0300

    restructure project

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,14 @@
 import type {NextPage} from 'next'
+import Image from 'next/image'
 import {MainLayout} from "../components/MainLayout";
+import styles from "../styles/home.module.scss";
 
 const Home: NextPage = () => {
     return (
         <MainLayout title={"Create Next App"}>
+          <div className={styles.mainBG}>
+            <Image src={"/assets/"}></Image>
+          </div>
             <div>
                 <h1>NEXT</h1>
             </div>
