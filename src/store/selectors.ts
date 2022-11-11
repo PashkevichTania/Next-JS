b1bb14f0cf6b3c9b884d01526c615b1dd50d3d9b
Pashkevich Tania <pashkevichtania2004@gmail.com>
2022-11-11 17:48:09 +0300

    refactor

--- src/store/selectors.ts
+++
@@ -1,6 +0,0 @@
-import { AppState } from "./store"
-
-export const filtersSelector = (state: AppState) => state.filters
-export const authSelector = (state: AppState) => state.auth
-export const gameEditSelector = (state: AppState) => state.gameEdit.game
-export const gameEditLoadingSelector = (state: AppState) => state.gameEdit.isLoading
