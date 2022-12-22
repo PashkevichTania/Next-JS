d677967f3a4d5835048a757fd2f5004c2ce397e1
pashkevichTania <pashkevichtania2004@gmail.com>
2022-12-22 15:43:27 +0300

    fix lint

--- src/pages/api/games/index.ts
+++ src/pages/api/games/index.ts
@@ -1,7 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next"
 import { GameData } from "@/utils/intefaces"
-import { addGame, getGameData } from "@/server/databaseUtils"
+import { getGameData } from "@/server/databaseUtils"
 
 type Response = {
   result: GameData[] | GameData
